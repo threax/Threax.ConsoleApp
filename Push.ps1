a5c404dadff615a71c66791e7dde539efe7cdd37
Andrew Piper <andrew@threax.com>
2021-05-12 15:22:49 -0400

    Added push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.ConsoleApp.git
+git push https://github.com/threax/Threax.ConsoleApp.git --tags
+git push https://gitlab.com/threax-software/Threax.ConsoleApp.git
+git push https://gitlab.com/threax-software/Threax.ConsoleApp.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.ConsoleApp
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.ConsoleApp --tags
