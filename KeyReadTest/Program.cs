b663032c48b0b5ceb3a132e4051485c0ca61c6e4
Andrew Piper <andrew@threax.com>
2021-06-03 08:39:36 -0400

    Added a function to read console input securely. Thanks huobazi https://gist.github.com/huobazi/1039424

---
+++ KeyReadTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Threax.ConsoleApp;
+
+namespace KeyReadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter something");
+            var password = SecureConsole.ReadLineMasked();
+            Console.WriteLine($"You entered: {password}");
+        }
+    }
+}
---
+++ Threax.ConsoleApp/SecureConsole.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threax.ConsoleApp
+{
+    public class SecureConsole
+    {
+        /// <summary>
+        /// Gets the console password.
+        /// </summary>
+        /// <remarks>
+        /// Thanks to huobazi from https://gist.github.com/huobazi/1039424 for this code.
+        /// </remarks>
+        /// <returns></returns>
+        public static string ReadLineMasked()
+        {
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    break;
+                }
+
+                if (cki.Key == ConsoleKey.Backspace)
+                {
+                    if (sb.Length > 0)
+                    {
+                        Console.Write("\b\0\b");
+                        sb.Length--;
+                    }
+                }
+
+                if (!char.IsControl(cki.KeyChar))
+                {
+                    Console.Write('*');
+                    sb.Append(cki.KeyChar);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
